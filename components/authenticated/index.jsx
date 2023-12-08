a56950ea1b5dacfb9a962e369888d675f6e838ee
itamarpambo <itamarpambo751@gmail.com>
2023-12-08 15:26:06 +0100

    Signout

--- components/authenticated/index.jsx
+++ components/authenticated/index.jsx
@@ -1,5 +1,5 @@
-import { Box, Flex, Heading, Image, Text } from "@chakra-ui/react";
-import { useSession } from "next-auth/react"
+import { Box, Button, Flex, Heading, Image, Text } from "@chakra-ui/react";
+import { signOut, useSession } from "next-auth/react"
 
 export default function Authenticated() {
     const { data: session } = useSession()
@@ -14,15 +14,23 @@ export default function Authenticated() {
                 <Heading>
                     <Text fontSize="2rem" fontWeight="400">My account</Text>
                 </Heading>
-                <Flex alignItems="center" justifyContent="flex-start" gap="1rem" background="gray.100" padding="50px" borderRadius="10px">
-                    <Box width="100px" height="100px" borderRadius="50%" overflow="hidden">
-                        <Image src={session.user.image} objectFit="cover" width="100%" height="100%"/>
-                    </Box>
-                    <Box>
-                        <Text fontSize="2rem">{session.user.name}</Text>
-                        <Text>{session.user.email}</Text>
-                    </Box>
-                </Flex>
+                <Box background="gray.100" padding="50px" borderRadius="10px">
+                    <Flex alignItems="center" justifyContent="flex-start" gap="1rem">
+                        <Box width="100px" height="100px" borderRadius="50%" overflow="hidden">
+                            <Image src={session.user.image} objectFit="cover" width="100%" height="100%"/>
+                        </Box>
+                        <Box>
+                            <Text fontSize="2rem">{session.user.name}</Text>
+                            <Text>{session.user.email}</Text>
+                        </Box>
+                    </Flex>
+                    <Button onClick={() => signOut()} marginTop="15px" bgGradient="linear(to right, #D31671 0%, #F65B99 50%, #D31671 100%)"
+                        color="gray.200" _hover={{
+                        bgColor: "pink.400"
+                    }}>
+                        Signout
+                    </Button>
+                </Box>
             </Box>
         </Flex>
     )
--- next.config.js
+++ next.config.js
@@ -10,7 +10,6 @@ const nextConfig = {
             }
         ],
     }
-
 }
 
 module.exports = nextConfig
